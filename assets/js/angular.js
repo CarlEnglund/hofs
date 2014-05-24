9c758b5383013aa2b20397a292563d5eb79c64c4
Carl <englund92@gmail.com>
2014-05-24 11:32:07 +0200

    added angular file

---
+++ assets/js/angular.js
@@ -0,0 +1,18 @@
+function TodoCtrl($scope) {
+  $scope.friends = [
+    {img: "assets/img/profile/mattias.jpg",
+    text:'Mattias Palmgren', 
+     done:false},
+    {img: "assets/img/profile/johan.jpg",
+     text:'Mattias Palmgren', 
+     done:false},
+     {
+      img: "assets/img/profile/carl.jpg",
+      text:'Mattias Palmgren', 
+     done:false},
+    {
+     img: "assets/img/profile/carl.jpg",
+     text:'Mattias Palmgren', 
+     done:false}];
+ };
+ 
